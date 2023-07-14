612f395747c79260e66d85862c23bf57acb87fb1
deAndros <andresgabriel.92@gmail.com>
2023-07-14 16:03:46 -0300

    Fix a generateUserErrorInfo

--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -1,82 +1,82 @@
-const { generateToken } = require("../utils/jwt");
-const { createHash, isValidPassword } = require("../utils/bcryptHash");
-const { usersService, cartsService } = require("../services");
-const { CustomError } = require("../utils/customErrors/Error.custom.class");
+const { generateToken } = require('../utils/jwt')
+const { createHash, isValidPassword } = require('../utils/bcryptHash')
+const { usersService, cartsService } = require('../services')
+const { CustomError } = require('../utils/customErrors/Error.custom.class')
 const {
   generateUserErrorInfo,
-} = require("../utils/customErrors/errorInfo.custom");
-const { errorCodes } = require("../utils/customErrors/errorCodes.custom");
+} = require('../utils/customErrors/errorInfo.custom')
+const { errorCodes } = require('../utils/customErrors/errorCodes.custom')
 
 class SessionsController {
   login = async (request, response, next) => {
     try {
-      const { email, password } = request.body;
+      const { email, password } = request.body
 
       if (!email || !password)
         CustomError.createError({
-          name: "Login failed",
-          cause: generateUserErrorInfo({ email, password }),
-          message: "El email y el password son obligatorios",
+          name: 'Login failed',
+          cause: generateUserErrorInfo({ email, password }, 'login'),
+          message: 'El email y el password son obligatorios',
           code: errorCodes.INVALID_TYPE_ERROR,
-        });
+        })
 
-      const userFromDB = await usersService.getByEmail(email);
+      const userFromDB = await usersService.getByEmail(email)
 
       if (!userFromDB)
         CustomError.createError({
-          name: "Login failed",
+          name: 'Login failed',
           cause: generateUserErrorInfo({ email, password }),
-          message: "No existe un usuario con el correo indicado",
+          message: 'No existe un usuario con el correo indicado',
           code: errorCodes.INVALID_CREDENTIALS_ERROR,
-        });
+        })
 
       if (!isValidPassword(password, userFromDB))
         CustomError.createError({
-          name: "Login failed",
-          cause: generateUserErrorInfo({ email, password }),
-          message: "La contraseña ingresada es inválida",
+          name: 'Login failed',
+          cause: generateUserErrorInfo({ email, password }, 'login'),
+          message: 'La contraseña ingresada es inválida',
           code: errorCodes.INVALID_CREDENTIALS_ERROR,
-        });
+        })
 
       const {
         _id,
         password: dbPassword,
         __v,
         ...userMetadata
-      } = userFromDB.toObject();
+      } = userFromDB.toObject()
 
-      const accessToken = generateToken(userMetadata);
+      const accessToken = generateToken(userMetadata)
 
       response
-        .cookie("accessToken", accessToken, {
+        .cookie('accessToken', accessToken, {
           maxAge: 60 * 60 * 10000,
           httpOnly: true,
         })
-        .redirect("/products");
+        .redirect('/products')
     } catch (error) {
-      next(error);
+      next(error)
     }
-  };
+  }
 
   logout = async (request, response) => {
     try {
-      response.clearCookie("accessToken").redirect("/login");
+      response.clearCookie('accessToken').redirect('/login')
     } catch (error) {
       response.status(500).send({
-        status: "error",
-        message: "Se produjo un error al desloguearse",
-      });
+        status: 'error',
+        message: 'Se produjo un error al desloguearse',
+      })
     }
-  };
+  }
 
   register = async (request, response, next) => {
     try {
       const { firstName, lastName, email, age, password, isAdmin } =
-        request.body;
+        request.body
 
       if (!firstName || !lastName || !email || !password)
         CustomError.createError({
-          name: "Registration failed",
+          name: 'Registration failed',
           cause: generateUserErrorInfo(
             {
               firstName,
@@ -84,18 +84,18 @@ class SessionsController {
               email,
               password,
             },
-            "register"
+            'register'
           ),
           message:
-            "El email, el nombre, el apellido y el password son obligatorios",
+            'El email, el nombre, el apellido y el password son obligatorios',
           code: errorCodes.INVALID_TYPE_ERROR,
-        });
+        })
 
-      const emailExists = await usersService.getByEmail(email);
+      const emailExists = await usersService.getByEmail(email)
 
       if (emailExists)
         CustomError.createError({
-          name: "Registration failed",
+          name: 'Registration failed',
           cause: generateUserErrorInfo(
             {
               firstName,
@@ -103,83 +103,83 @@ class SessionsController {
               email,
               password,
             },
-            "register"
+            'register'
           ),
-          message: "Ya existe una cuenta con el email ingresado",
+          message: 'Ya existe una cuenta con el email ingresado',
           code: errorCodes.INVALID_CREDENTIALS_ERROR,
-        });
+        })
 
-      const newUserCart = await cartsService.create();
-      let newUser = await usersService.create(request.body, newUserCart);
+      const newUserCart = await cartsService.create()
+      let newUser = await usersService.create(request.body, newUserCart)
 
       const {
         _id,
         password: dbPassword,
         __v,
         ...newUserMetadata
-      } = newUser.toObject();
+      } = newUser.toObject()
 
       response.send({
-        status: "success",
+        status: 'success',
         newUser: newUserMetadata,
-      });
+      })
     } catch (error) {
-      next(error);
+      next(error)
     }
-  };
+  }
 
   restorePassword = async (request, response, next) => {
     try {
-      const { email, password } = request.body;
+      const { email, password } = request.body
 
       if (!email || !password)
         CustomError.createError({
-          name: "Password restoration failed",
+          name: 'Password restoration failed',
           cause: generateUserErrorInfo(
             {
               email,
               password,
             },
-            "restorePassword"
+            'restorePassword'
           ),
-          message: "El email y el password son obligatorios",
+          message: 'El email y el password son obligatorios',
           code: errorCodes.INVALID_TYPE_ERROR,
-        });
+        })
 
-      const userFromDB = await usersService.getByEmail(email);
+      const userFromDB = await usersService.getByEmail(email)
 
       if (!userFromDB)
         //TODO: Redireccionar a una página de error o arrojar un modal con un error
         CustomError.createError({
-          name: "Password restoration failed",
+          name: 'Password restoration failed',
           cause: generateUserErrorInfo(
             {
               email,
               password,
             },
-            "restorePassword"
+            'restorePassword'
           ),
-          message: "No existe un usuario con el email ingresado",
+          message: 'No existe un usuario con el email ingresado',
           code: errorCodes.INVALID_CREDENTIALS_ERROR,
-        });
+        })
 
-      userFromDB.password = password;
+      userFromDB.password = password
 
-      await usersService.update(userFromDB._id, userFromDB);
+      await usersService.update(userFromDB._id, userFromDB)
 
       //TODO: Redireccionar a la página de login
       response.status(200).send({
-        status: "success",
-        message: "Constaseña actualizada correctamente",
-      });
+        status: 'success',
+        message: 'Constaseña actualizada correctamente',
+      })
       /*TODO: Hacer un redirect a la página de login y arrojar una alerta indicando que la contraseña se actualizó correctamente*/
       //.redirect('/login')
     } catch (error) {
-      next(error);
+      next(error)
     }
-  };
+  }
 
   //TODO: Implementar gitHubLogin con JWT en este controlador
 }
 
-module.exports = new SessionsController();
+module.exports = new SessionsController()
--- src/utils/customErrors/errorInfo.custom.js
+++ src/utils/customErrors/errorInfo.custom.js
@@ -1,29 +1,30 @@
 exports.generateUserErrorInfo = (user, action) => {
-  let info = `One or more properties were incomplete or not valid.`;
+  let info = `One or more properties were incomplete or not valid.`
+
   switch (action) {
-    case "login":
-      info += `\n  * email: needs to be String, received ${user.email}`;
+    case 'login':
+      info += `\n  * email: needs to be String, received ${user.email}`
       if (!user.password)
-        info += "\n  * password: needs to be String but was not provided";
-      break;
-    case "restorePassword":
-      info += `\n  * email: needs to be String, received ${user.email}`;
+        info += '\n  * password: needs to be String but was not provided'
+      break
+    case 'restorePassword':
+      info += `\n  * email: needs to be String, received ${user.email}`
       if (!user.password)
-        info += "\n  * password: needs to be String but was not provided";
-      break;
-    case "register":
-      info += `\n  * email: needs to be String, received ${user.email}\n  * firstName: needs to be String, received ${user.firstName}\n  * lastName: needs to be String, received ${user.lastName}`;
-      break;
+        info += '\n  * password: needs to be String but was not provided'
+      break
+    case 'register':
+      info += `\n  * email: needs to be String, received ${user.email}\n  * firstName: needs to be String, received ${user.firstName}\n  * lastName: needs to be String, received ${user.lastName}`
+      break
     default:
-      break;
+      break
   }
-  return info;
-};
+  return info
+}
 
 exports.generatePoductErrorInfo = (product) => {
   return `One or more properties were incomplete or not valid.
         listado de requirimientos de propiedades del user:
         * first_name: needs to a String, received ${user.title}
         * last_name: needs to a String, received ${user.price}
-        * email: needs to a String, received ${user.category}`;
-};
+        * email: needs to a String, received ${user.category}`
+}
