66995dc6a35d3a16cb2745c163d9326830b7914e
deAndros <andresgabriel.92@gmail.com>
2023-08-16 11:51:51 -0300

    Fix al supertest de carritos

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,4 +1,4 @@
-const CustomRouter = require('./customRouter.class')
+const CustomRouter = require("./customRouter.class");
 
 const {
   getCarts,
@@ -10,40 +10,40 @@ const {
   deleteProductFromCart,
   deleteCart,
   generateTicket,
-} = require('../controllers/carts.controller.js')
+} = require("../controllers/carts.controller.js");
 
 class CartsRouter extends CustomRouter {
   init() {
-    this.get('/', ['ADMIN'], getCarts)
+    this.get("/", ["ADMIN"], getCarts);
 
-    this.get('/:cid([a-zA-Z0-9]+)', ['ADMIN'], getCartById)
+    this.get("/:cid([a-zA-Z0-9]+)", ["ADMIN"], getCartById);
 
-    this.post('/', ['ADMIN'], createCart)
+    this.post("/", ["ADMIN"], createCart);
 
-    this.post('/purchase/', ['USER', 'PREMIUM'], generateTicket)
+    this.post("/purchase/", ["USER", "PREMIUM"], generateTicket);
 
     this.post(
-      '/:cid([a-zA-Z0-9]+)/product/:pid([a-zA-Z0-9]+)',
-      ['USER', 'PREMIUM'],
+      "/:cid([a-zA-Z0-9]+)/product/:pid([a-zA-Z0-9]+)",
+      ["USER", "PREMIUM"],
       addProductToCart
-    )
+    );
 
-    this.put('/:cid([a-zA-Z0-9]+)', ['ADMIN'], updateCartProducts)
+    this.put("/:cid([a-zA-Z0-9]+)", ["ADMIN"], updateCartProducts);
 
     this.put(
-      '/:cid([a-zA-Z0-9]+)/product/:pid([a-zA-Z0-9]+)',
-      ['USER', 'PREMIUM'],
+      "/:cid([a-zA-Z0-9]+)/product/:pid([a-zA-Z0-9]+)",
+      ["USER", "PREMIUM"],
       updateProductQuantity
-    )
+    );
 
     this.delete(
-      '/:cid([a-zA-Z0-9]+)/product/:pid([a-zA-Z0-9]+)',
-      ['USER', 'PREMIUM'],
+      "/:cid([a-zA-Z0-9]+)/product/:pid([a-zA-Z0-9]+)",
+      ["USER", "PREMIUM"],
       deleteProductFromCart
-    )
+    );
 
-    this.delete('/:cid([a-zA-Z0-9]+)', ['ADMIN'], deleteCart)
+    this.delete("/:cid([a-zA-Z0-9]+)", ["ADMIN"], deleteCart);
   }
 }
 
-module.exports = new CartsRouter()
+module.exports = new CartsRouter();
